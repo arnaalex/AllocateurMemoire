23e847c4ec886374f455b17dd83f7d3def760021
Alexia Arnaud <alexia.arnaud@ensimag.grenoble-inp.fr>
2015-01-19 12:00:16 +0100

    init

---
+++ build/CMakeFiles/alloctest.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/user/1/.base/arnaalex/home/ensimag-allocateur/src/alloctest.cc" "/user/1/.base/arnaalex/home/ensimag-allocateur/build/CMakeFiles/alloctest.dir/src/alloctest.cc.o"
+  "/user/1/.base/arnaalex/home/ensimag-allocateur/tests/test_bf.cc" "/user/1/.base/arnaalex/home/ensimag-allocateur/build/CMakeFiles/alloctest.dir/tests/test_bf.cc.o"
+  "/user/1/.base/arnaalex/home/ensimag-allocateur/tests/test_buddy.cc" "/user/1/.base/arnaalex/home/ensimag-allocateur/build/CMakeFiles/alloctest.dir/tests/test_buddy.cc.o"
+  "/user/1/.base/arnaalex/home/ensimag-allocateur/tests/test_cff.cc" "/user/1/.base/arnaalex/home/ensimag-allocateur/build/CMakeFiles/alloctest.dir/tests/test_cff.cc.o"
+  "/user/1/.base/arnaalex/home/ensimag-allocateur/tests/test_generic.cc" "/user/1/.base/arnaalex/home/ensimag-allocateur/build/CMakeFiles/alloctest.dir/tests/test_generic.cc.o"
+  "/user/1/.base/arnaalex/home/ensimag-allocateur/tests/test_run_cpp.cc" "/user/1/.base/arnaalex/home/ensimag-allocateur/build/CMakeFiles/alloctest.dir/tests/test_run_cpp.cc.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  "/user/1/.base/arnaalex/home/ensimag-allocateur/build/CMakeFiles/allocphy.dir/DependInfo.cmake"
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /user/1/.base/arnaalex/home/ensimag-allocateur
+# Build directory: /user/1/.base/arnaalex/home/ensimag-allocateur/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+ADD_TEST(AllTestsAllocator "alloctest")

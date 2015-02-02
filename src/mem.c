35d5f6623cee51e5138a75a582978d1fc56852c1
Brazouskaya Darya <Darya.Brazouskaya@ensimag.imag.fr>
2015-02-02 13:52:59 +0100

    olo

--- src/mem.c
+++ src/mem.c
@@ -42,79 +42,78 @@ mem_init()
 mem_alloc(unsigned long size)
 {
 
-  if(size<=0)
-    return NULL;
+    if(size<=0)
+        return NULL;
 
-  int bloc = ceil(log(size)/log(2));
-  int alloc_min = ceil(log( sizeof(void*))/log(2)); 
-  if (bloc<alloc_min){
-    bloc=alloc_min;
-  }
-  void* res = NULL;
-  int i = bloc; 
+    int bloc = ceil(log(size)/log(2));
+    int alloc_min = ceil(log( sizeof(void*))/log(2)); 
+    if (bloc<alloc_min){
+        bloc=alloc_min;
+    }
+    void* res = NULL;
+    int i = bloc; 
 
-   
+
     while((i<BUDDY_MAX_INDEX+1) && TZL[i]==NULL)
-      i++;
-    
+        i++;
+
     while(  i<BUDDY_MAX_INDEX+1 && i!=bloc){
-      void* un = TZL[i];
-      void* deux = (void*)((uintptr_t)TZL[i]+(1<<(i-1)));
-      TZL[i] = *((void**)TZL[i]);
-      i=i-1;
-      *(void**)un = deux;
-      *(void**)deux = TZL[i];
-      TZL[i] = un; 
+        void* un = TZL[i];
+        void* deux = (void*)((uintptr_t)TZL[i]+(1<<(i-1)));
+        TZL[i] = *((void**)TZL[i]);
+        i=i-1;
+        *(void**)un = deux;
+        *(void**)deux = TZL[i];
+        TZL[i] = un; 
     }
-        if(i==bloc){
-      res =  TZL[i];
-      TZL[i] = *((void **)TZL[i]);
+    if(i==bloc){
+        res =  TZL[i];
+        TZL[i] = *((void **)TZL[i]);
 
     }
+
     return res;  
 }
 
     int 
 mem_free(void *ptr, unsigned long size)
 {
-  
-  if(ptr<zone_memoire || (ptr+size)>(zone_memoire+ALLOC_MEM_SIZE))
-    return 1;
 
-  int i = ceil(log(size)/log(2));
-  int alloc_min = ceil(log( sizeof(void*))/log(2)); 
-  if (i<alloc_min){
-    i=alloc_min;
-  }
-  bool found = true;
-  void * mon_ad = ptr;
-  void* p;
-  void* adr_comp;
-  void * mon_ad_correct;
-  while(i< BUDDY_MAX_INDEX && found){
-    mon_ad_correct =(void*)( (uintptr_t)mon_ad -(uintptr_t)zone_memoire); 
-    adr_comp =(void*)(((uintptr_t)mon_ad_correct^(1<<(i)))+zone_memoire);
+    if(ptr<zone_memoire || ptr>(zone_memoire+ALLOC_MEM_SIZE) || (ptr+size)>(zone_memoire+ALLOC_MEM_SIZE))
+        return 1;
 
-    p = TZL[i];
-    found = adr_comp == p ;
-    bool first = found;
-    while(p!=NULL && !found){
-      if(adr_comp == *(void**)p){
-	found = true;
-      }
-      else{
-	p = *(void**)p;
-      }
+    int i = ceil(log(size)/log(2));
+    int alloc_min = ceil(log( sizeof(void*))/log(2)); 
+    if (i<alloc_min){
+        i=alloc_min;
     }
-    if(found){
-      i++;
-      if(!first)
-      *(void**)p = *(void**)adr_comp;
-      else
-	TZL[i-1]=*(void**)p;
-      mon_ad = (mon_ad<adr_comp)?mon_ad:adr_comp;
+    bool found = true;
+    void * mon_ad = ptr;
+    void* p;
+    void* adr_comp;
+    void * mon_ad_correct;
+    while(i< BUDDY_MAX_INDEX && found){
+        mon_ad_correct =(void*)( (uintptr_t)mon_ad -(uintptr_t)zone_memoire); 
+        adr_comp =(void*)(((uintptr_t)mon_ad_correct^(1<<(i)))+zone_memoire);
+        p = TZL[i];
+        found = adr_comp == p ;
+        bool first = found;
+
+        while(p!=NULL && !found){
+            if(adr_comp == *(void**)p)
+                found = true;
+            else
+                p = *(void**)p;   
+        }
+        if(found){
+            i++;
+            if(!first)
+                *(void**)p = *(void**)adr_comp;
+            else
+                TZL[i-1]=*(void**)p;
+            mon_ad = (mon_ad<adr_comp)?mon_ad:adr_comp;
+        }
     }
-  }
 
     *(void**)mon_ad = TZL[i];
     TZL[i] = mon_ad;

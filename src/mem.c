b673d1195f2195681a97a79085a371f9254bbdc3
Brazouskaya Darya <Darya.Brazouskaya@ensimag.imag.fr>
2015-02-02 13:21:23 +0100

    correction

--- src/mem.c
+++ src/mem.c
@@ -15,8 +15,6 @@
 /** squelette du TP allocateur memoire */
 
 void *zone_memoire = 0;
-
-/* ecrire votre code ici */
 void *TZL[BUDDY_MAX_INDEX+1];
 
     int 
@@ -30,7 +28,6 @@ mem_init()
         return -1;
     }
 
-    /* ecrire votre code ici */
     //Initialisation de TZL 
     for(int i=0; i<BUDDY_MAX_INDEX; ++i){
         TZL[i]=NULL;
@@ -44,8 +41,7 @@ mem_init()
     void *
 mem_alloc(unsigned long size)
 {
-  // printf("start\n");
-    /*  ecrire votre code ici */
+
   if(size<=0)
     return NULL;
 
@@ -54,7 +50,6 @@ mem_alloc(unsigned long size)
   if (bloc<alloc_min){
     bloc=alloc_min;
   }
-  //printf("bloc %d\n", bloc);
   void* res = NULL;
   int i = bloc; 
 
@@ -64,8 +59,6 @@ mem_alloc(unsigned long size)
     
     while(  i<BUDDY_MAX_INDEX+1 && i!=bloc){
       void* un = TZL[i];
-      //int k = 1<<(i-1); 
-      //printf("%d\n",k);
       void* deux = (void*)((uintptr_t)TZL[i]+(1<<(i-1)));
       TZL[i] = *((void**)TZL[i]);
       i=i-1;
@@ -78,24 +71,16 @@ mem_alloc(unsigned long size)
       TZL[i] = *((void **)TZL[i]);
 
     }
-	//	for(int j=0; j<BUDDY_MAX_INDEX+1; j++)
-	//printf("%p ", TZL[j]);
-	//printf("\n");
-    
-	//  if (res!=NULL) printf("\neeee %lu %d %p %p\n", size, i,res, zone_memoire);
-	//else printf("null %lu\n", size);
-	//fflush(stdout);
     return res;  
 }
 
     int 
 mem_free(void *ptr, unsigned long size)
 {
-    /* ecrire votre code ici */
   
-if(ptr<zone_memoire || ptr>(zone_memoire+ALLOC_MEM_SIZE))
+  if(ptr<zone_memoire || (ptr+size)>(zone_memoire+ALLOC_MEM_SIZE))
     return 1;
-//printf("olol %p %p %p %p\n",ptr, zone_memoire, ptr+size,zone_memoire+ALLOC_MEM_SIZE);
+
   int i = ceil(log(size)/log(2));
   int alloc_min = ceil(log( sizeof(void*))/log(2)); 
   if (i<alloc_min){
@@ -105,12 +90,11 @@ if(ptr<zone_memoire || ptr>(zone_memoire+ALLOC_MEM_SIZE))
   void * mon_ad = ptr;
   void* p;
   void* adr_comp;
+  void * mon_ad_correct;
   while(i< BUDDY_MAX_INDEX && found){
-    // adr_comp =(void*)(((uintptr_t)mon_ad^(1<<(i))));
- adr_comp =(void*)(((uintptr_t)mon_ad^(1<<(i))));
-    // if((uintptr_t)mon_ad&(1<<i)) 
-    // if(i==4 || i==12 || i>=15 )
-    //adr_comp =(void*)(((uintptr_t)mon_ad^(1<<i)^(1<<(i+1))));
+    mon_ad_correct =(void*)( (uintptr_t)mon_ad -(uintptr_t)zone_memoire); 
+    adr_comp =(void*)(((uintptr_t)mon_ad_correct^(1<<(i)))+zone_memoire);
+
     p = TZL[i];
     found = adr_comp == p ;
     bool first = found;
@@ -134,9 +118,7 @@ if(ptr<zone_memoire || ptr>(zone_memoire+ALLOC_MEM_SIZE))
 
     *(void**)mon_ad = TZL[i];
     TZL[i] = mon_ad;
-for(int j=0; j<BUDDY_MAX_INDEX+1; j++)
-	  printf("%p ", TZL[j]);
-	printf("\n");
+
     return 0;
 }
 
@@ -144,7 +126,6 @@ for(int j=0; j<BUDDY_MAX_INDEX+1; j++)
     int
 mem_destroy()
 {
-    /* ecrire votre code ici */
     free(zone_memoire);
     zone_memoire = 0;
     return 0;
